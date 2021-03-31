fe0df9ea962687b533dece486d1ca302ab265744
Md Zahid Hassan <46865100+zh-cse18@users.noreply.github.com>
2021-03-31 09:11:32 +0600

    worked on searched result with keyword wise

--- core/qtec_app/views.py
+++ core/qtec_app/views.py
@@ -1,3 +1,5 @@
+from datetime import date, timedelta
+
 from django.db.models import Count
 from django.shortcuts import render, redirect
 
@@ -21,16 +23,57 @@ def search(request):
 def search_result(request):
     name = []
     key = []
+    final_result = {}
     result = SearchResult.objects.all()
-    for unique_user in result:
-        if unique_user.keyword not in key:
-            key.append(unique_user.keyword)
-        if unique_user.user_name not in name:
-            name.append(unique_user.user_name)
+    if request.method == 'POST':
+        start_date = request.POST.get('start_date')
+        end_date = request.POST.get('end_date')
+        yestrday = request.POST.get('yesterday')
+        lastweek = request.POST.get('lastweek')
+        print(yestrday)
+        print(lastweek)
+
+        if yestrday:
+            today =date.today()
+            yesterday = today - timedelta(days=1)
+            result = SearchResult.objects.filter(searched_at=yesterday)
+            print(yesterday)
+
+        if lastweek:
+            today = date.today()
+            lastday = today - timedelta(days=6)
+            result = SearchResult.objects.filter(searched_at__gte=today, searched_at__lte=lastday)
+            print(lastday)
+
+        if start_date and end_date:
+            result = SearchResult.objects.filter(searched_at__gte=start_date, searched_at__lte=end_date)
+
+        all_selected_word = request.POST.getlist('select_word')
+        all_selected_user = request.POST.getlist('select_user')
+        if all_selected_word:
+            for each_word in all_selected_word:
+                result_eachword = SearchResult.objects.filter(keyword=each_word)
+                final_result[each_word] = result_eachword
+
+        context = {
+        'result': result,
+        'key': key,
+        'name': name,
+        'final_result': final_result
+        }
+        return render(request, 'search_result.html', context)
+
+    else:
+        for unique_user in result:
+            if unique_user.keyword not in key:
+                key.append(unique_user.keyword)
+            if unique_user.user_name not in name:
+                name.append(unique_user.user_name)
+
     context = {
         'result': result,
         'key': key,
-        'name': name
-
+        'name': name,
+        'final_result': final_result
     }
     return render(request, 'search_result.html', context)
