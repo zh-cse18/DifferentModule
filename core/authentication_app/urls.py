f79f6603e85a1be620875de35020597013e7bebc
Md Zahid Hassan <46865100+zh-cse18@users.noreply.github.com>
2021-06-17 11:14:42 +0600

    Added account confirmation mail.

--- core/authentication_app/urls.py
+++ core/authentication_app/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from .views import user_login, register, user_logout, change_password
-from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetCompleteView,\
-    PasswordResetConfirmView
+from .views import user_login, register, user_logout, change_password, activate_account
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView
 urlpatterns = [
     path('login/', user_login, name="login"),
     path('register/', register, name="registration"),
     path('logout/', user_logout, name="logout"),
     path('change_pass/', change_password, name="changepass"),
 
-    path('reset/password/', PasswordResetView.as_view(template_name='reset_pass.html'), name="password_reset"),
-    path('reset/password/done/', PasswordResetDoneView.as_view(template_name='reset_pass_done.html'), name="reset_pass_done"),
-    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name="password_reset_confirm"),
-    path('reset/done/', PasswordResetView.as_view(template_name='password_reset_complete.html'), name="password_reset_complete"),
-
+    #pass reset related
+    path('reset/password/', PasswordResetView.as_view(template_name='authentication/reset_pass.html'), name="password_reset"),
+    path('reset/password/done/', PasswordResetDoneView.as_view(template_name='authentication/reset_pass_done.html'), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='authentication/password_reset_confirm.html'), name="password_reset_confirm"),
+    path('reset/done/', PasswordResetView.as_view(template_name='authentication/password_reset_complete.html'), name="password_reset_complete"),
+    path('activate/<uidb64>/<token>/', activate_account, name="activate_account"),
 ]
--- core/authentication_app/views.py
+++ core/authentication_app/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404, HttpResponse
-from django.contrib.auth import login, authenticate, logout, update_session_auth_hash
+from django.contrib.auth import login, authenticate, logout, update_session_auth_hash, get_user_model
 from django.shortcuts import render, redirect
 from .forms import SignUpForm
 from django.contrib.auth.forms import PasswordChangeForm
@@ -9,6 +9,10 @@ from django.contrib import messages
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+UserModel =get_user_model()
 
 
 def user_login(request):
@@ -23,24 +27,55 @@ def user_login(request):
                 login(request, auth)
                 return redirect('search')
 
-    return render(request, 'login.html', )
+    return render(request, 'authentication/login.html', )
 
 
 def user_logout(request):
     logout(request)
     return redirect('search')
 
+#will send a mail when account is created with the mail account
+# def register(request):
+#     if request.method == 'POST':
+#         form = SignUpForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             current_site = get_current_site(request)
+#             mail_subject = 'An account is created'
+#             message = render_to_string('authentication/user_mail.html', {
+#                 'user': user,
+#                 'domain': current_site.domain
+#             })
+#             send_mail = form.cleaned_data.get('email')
+#             email = EmailMessage(mail_subject, message, to=[send_mail])
+#             email.send()
+#             messages.success(request, "Send a mail")
+#             username = form.cleaned_data.get('username')
+#             raw_password = form.cleaned_data.get('password1')
+#             user = authenticate(username=username, password=raw_password)
+#             login(request, user)
+#             return redirect('search')
+#
+#     else:
+#         form = SignUpForm()
+#     return render(request, 'authentication/register.html', {'form': form})
 
+
+#will send a confirmation mail for checking the given mail owner real or fake
 def register(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
             current_site = get_current_site(request)
             mail_subject = 'An account is created'
-            message = render_to_string('user_mail.html', {
+            message = render_to_string('authentication/user_mail.html', {
                 'user': user,
-                'domain': current_site.domain
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user)
             })
             send_mail = form.cleaned_data.get('email')
             email = EmailMessage(mail_subject, message, to=[send_mail])
@@ -54,7 +89,25 @@ def register(request):
 
     else:
         form = SignUpForm()
-    return render(request, 'register.html', {'form': form})
+    return render(request, 'authentication/register.html', {'form': form})
+
+
+def activate_account(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = UserModel._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError):
+        user = None
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, "Your account is activated")
+        return redirect('login')
+
+    else:
+        messages.warning(request, "Activation link is invalid")
+        return redirect('registration')
+
 
 
 def change_password(request):
@@ -73,4 +126,4 @@ def change_password(request):
     else:
         form = PasswordChangeForm(user=request.user)
 
-    return render(request, 'change_password.html', {'form': form})
+    return render(request, 'authentication/change_password.html', {'form': form})
